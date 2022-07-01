f2f9b304e92d6a975fb51657dbfd108f0e53986f
Habib-aw <habib.awahid01@gmail.com>
2022-07-01 12:16:12 +0100

    Dhul-Hijjah-uodates

--- RUN.py
+++ RUN.py
@@ -10,7 +10,6 @@ from SalahInfo import *
 # from Bot import *
 from Footer import *
 from Slideshow import *
-from PIL import ImageTk,Image
 from salahTimer import Timer
 # Other
 from Settings import background,foreground,salahTitles,fontStyle,JummahTimes,BMA_logoLength,BMA_logoWidth,BMA_logoPositioningRelx,BMA_logoPositioningRely,x2,x1,x,y1,y,jummahXpos,jummahYpos,jummahTitleXpos,jummahTitleYpos,salahContainerFont
@@ -43,13 +42,50 @@ logo_pic = baitulMamurLogo.resize((BMA_logoWidth,BMA_logoLength),Image.ANTIALIAS
 new_logo = ImageTk.PhotoImage(logo_pic)
 Label(salahContinerframe,image=new_logo).place(relx=BMA_logoPositioningRelx,rely=BMA_logoPositioningRely,anchor='center')
 
-s1 = Slide(root,content="",frame=salahContinerframe,time=10)
-s2 = Slide(root,title="Donations",content="Please donate to the masjid using the charity box or card machine near the entrance/exit\nor\nBy using online transfer\nOrganisation name: Baitul Mamur Academy\nAcc no. 31643290\nSort code: 40-01-18",contentFont=60)
-s1.packSlide()
+s1 = Slide(root,
+content="",
+frame=salahContinerframe,
+time=10
+)
+
+s2 = Slide(root,
+title="Donations",
+content="Please donate to the masjid using the charity box or card machine near the entrance/exit\nor\nBy using online transfer\nOrganisation name: Baitul Mamur Academy\nAcc no. 31643290\nSort code: 40-01-18",
+contentFont=60
+)
+
 # s3 = Slide(root,title="EID JAMA'AH",content="1st Jama'ah: 7:00 AM\n\n2nd Jama'ah: 8:30 AM\n\n3rd Jama'ah: 10:00 AM",contentFont=100,bg='black')
-slideshow.addAll([s1,s2])	
+	
 # p = PostRamadan(root,slideshow)
 # r = Ramadan(slideshow,root)
+s3 = Slide(root,
+title="'Virtues Of Dhul-Hijjah'",
+content="""There are no days in the year more beloved to Allah swt than the first ten days of Dhul-Hijjah and the last ten nights of Ramadan, as they combine acts of worship in a way unlike any other time. The Prophet Pbuh said, 'There is no deed that is better in the sight of Allah or more greatly rewarded than a good deed done in the (first) ten days of Al-Adha'. It was asked, 'Not even Jihad for the sake of Allah?' The Prophet Pbuh replied, 'Not even Jihad for the sake of Allah, unless a man goes out himself for Jihad taking his wealth with him and does not come back with anything.' 
+[Al Bukhari]""",
+contentFont=46,
+time=15,
+bg="white",
+fg=background    
+)
+s4 = Slide(root,
+title="'Maximising Your Rewards'",  
+content="""The first ten days of Dhul Hijjah are almost upon us and we don’t want to miss out on a single blessing. Even if you are not going on Hajj, there’s plenty you can do to make the most of Dhul Hijjah at home.
+- Perform Dhikr & Takbeer 
+- Standing In Night Prayer
+- Fasting all 9 days especially Day Of Arafah
+- Make Sincere Repentance
+- Perform The Hajj Pilgrimage
+- Give A Prophetic Qurbani
+- Return to Book of Allah (Quran)
+- Give Sadaqah & Charity""",
+contentFont=43,
+wraplength=root.winfo_screenwidth()-100,
+time=15,
+bg="white",
+fg=background
+)
+s1.packSlide()
+slideshow.addAll([s1,s2,s3,s4])
 
 t = Timer(root,salahInfo.salahTimesObj,[f,slideshow],changes,announcements,timeChanges,salahLabels,None)
 slideshow.redoTimes()
--- Slide.py
+++ Slide.py
@@ -2,11 +2,11 @@ from datetime import datetime,timedelta
 from tkinter import Frame,Label
 from Settings import background, foreground
 class Slide:
-	def __init__(self,root, content,contentFont=35,title=None,titleFont=55,whatTime=None,time=7,howLong=5,frame=None,paddingCtop=10,fg=foreground,bg=background,image=None,announce=False):
+	def __init__(self,root, content,contentFont=35,title=None,titleFont=55,whatTime=None,time=7,howLong=5,frame=None,paddingCtop=10,fg=foreground,bg=background,image=None,announce=False,wraplength=None):
 		if frame !=None:
 			self.frame = frame
 		else:
-			self.createFrame(root,content,contentFont,title,titleFont,paddingCtop,fg,bg,image)
+			self.createFrame(root,content,contentFont,title,titleFont,paddingCtop,fg,bg,image,wraplength)
 		if whatTime == None:
 			self.time = time
 		else:
@@ -17,18 +17,22 @@ class Slide:
 		self.frame.pack()
 	def forgetP(self):
 		self.frame.pack_forget()
-	def createFrame(self,root,content,contentFont,title,titleFont,paddingCtop,fg,bg,image):
+	def createFrame(self,root,content,contentFont,title,titleFont,paddingCtop,fg,bg,image,wraplength):
 		self.frame =Frame(root,bg=bg,width=root.winfo_screenwidth())
 		self.inFrame = Frame(self.frame,bg=background)
 		self.inFrame.pack(side='bottom')
 		if image ==None:
-			self.content = Label(self.inFrame,text=content,font=('Arial',contentFont),fg=fg,bg=bg,wraplength=root.winfo_screenwidth()-200)
+			if wraplength == None:
+				wraplength=root.winfo_screenwidth()-200
+			self.content = Label(self.inFrame,text=content,font=('Arial',contentFont),fg=fg,bg=bg,wraplength=wraplength)
 			self.content.pack(ipadx=1000,ipady=paddingCtop)
 		elif content == None:
 			self.imageLabel = Label(self.inFrame,image=image)
 			self.imageLabel.pack()
 		else:
-			self.content = Label(self.inFrame,text=content,font=('Arial',contentFont),fg=fg,bg=bg,wraplength=root.winfo_screenwidth()-700)
+			if wraplength == None:
+				wraplength=root.winfo_screenwidth()-700
+			self.content = Label(self.inFrame,text=content,font=('Arial',contentFont),fg=fg,bg=bg,wraplength=wraplength)
 			self.imageLabel = Label(self.inFrame,image=image)
 			self.imageLabel.pack(side='left')
 			self.content.pack(side='right')
