126823988e3e0e99db9c134cae4e2123696a673e
Habib-aw <habib.awahid01@gmail.com>
2023-07-18 19:33:44 +0100

    Update update.py

--- update.py
+++ update.py
@@ -2,11 +2,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import json
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("Mosque-dashboard-application/serviceAccountKey.json")
 
 firebase_admin.initialize_app(cred,{
     "databaseURL":"https://playground-fdda0-default-rtdb.firebaseio.com"
 })
 
-with open('db.json', 'w', encoding='utf-8') as f:
+with open('Mosque-dashboard-application/db.json', 'w', encoding='utf-8') as f:
     json.dump(db.reference().get(), f, ensure_ascii=False, indent=4)
